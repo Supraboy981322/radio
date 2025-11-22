e2279b4606afd17e65523f93ad570841588dd43a
zane <97927259+Supraboy981322@users.noreply.github.com>
2025-11-22 14:13:21 -0600

    prep for new json setup

--- src/web.go
+++ src/web.go
@@ -142,3 +142,87 @@ func buildJSONlibrary() []byte {
 
 	return jsonByte
 }
+
+func buildJSONsettings() []byte {
+	res := []string{
+		"[", "  \"config\": {",
+	}
+	for keyRaw, valRaw := range config {
+		var key, valStr, object string
+		var ok, valBool bool;
+		var valInt int
+		if key, ok = keyRaw.(string); !ok {
+			log.Fatal("failed type assertion of key to string")
+		} else { object += "    \""+key+"\": " }
+
+		if valStr, ok = valRaw.(string); !ok {
+			if valInt, ok = valRaw.(int); !ok {
+				if valBool, ok = valRaw.(bool); !ok {
+					log.Fatal("failed type assertion of value to bool")
+				} else { object += strconv.FormatBool(valBool)+"," }
+			} else { object += strconv.Itoa(valInt)+"," }
+		} else { object += "\""+valStr+"\"," }
+
+		res = append(res, object)
+	}; res = delLastCharInSlice(res)
+
+	//close object
+	res = append(res, "  },")
+
+	//create library JSON object
+	res = append(res, "  \"library\":  [")
+
+	for nameRaw, pathRaw := range library {
+		object := "    [\n" 
+		var name, path string
+		var ok bool
+		if name, ok = nameRaw.(string); !ok {
+			log.Fatal("failed to type assert name of station")
+		}; object += "      \""+name+"\",\n"
+	
+		url := icecastDomain+"/"+name+".ogg"
+		object += "      \""+url+"\",\n"
+
+		if path, ok = pathRaw.(string); !ok {
+			log.Fatal("failed to type assert path of station")
+		}
+		object += "      \""+path+"\",\n"
+
+		object += "    ],"
+		res = append(res, object)
+	}
+	for nameRaw, urlRaw := range externalLib {
+		object := "    [\n" 
+		var name, url string
+		var ok bool
+		if name, ok = nameRaw.(string); !ok {
+			log.Fatal("failed to type assert name of station")
+		}; object += "      \""+name+"\",\n"
+
+		if url, ok = urlRaw.(string); !ok {
+			log.Fatal("failed to type assert path of station")
+		}
+		object += "      \""+url+"\"\n"
+		
+		object += "      null\n"
+
+		object += "    ],"
+		res = append(res, object)
+	}; delLastCharInSlice(res)
+	
+	res = append(res, "  ],")
+
+	//remove last char from last line (a comma) 
+	// and close json
+	res = delLastCharInSlice(res)
+	res = append(res, "]")
+
+	return []byte(strings.Join(res, "\n"))
+}
+
+func delLastCharInSlice(slice []string) []string {
+	lastItm := len(slice)-1
+	lastItmLen := len(slice[lastItm])
+	slice[lastItm] = slice[lastItm][:lastItmLen-1]
+	return slice
+}
